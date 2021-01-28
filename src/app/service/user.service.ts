473f320586d8d4e4e62ba60c28f3166dd81227fd
mattedamo <mattedamo@gmail.com>
2021-01-28 16:34:04 -0500

    a

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   private usersUrl: string;
 
   constructor(private http: HttpClient) {
-    this.usersUrl = 'http://backend-service:8080/API/users';
+    this.usersUrl = 'http://kube-worker:31000/API/users';
   }
 
   public findAll(): Observable<User[]> {
